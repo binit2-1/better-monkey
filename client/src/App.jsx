8ebe5c4ddea266575a89bf4b03a774d68c7c304c
binit <binitgupta.1711@gmail.com>
2025-10-25 00:09:11 +0530

    Update project structure and styling
    - Changed the document title to "bettermonkey" in index.html
    - Updated App component styling to use the 'bg-crust' class
    - Added Tailwind CSS configuration and custom color variables in index.css
    - Created a new Hompage component
    - Initialized Tailwind CSS configuration file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='w-screen h-screen bg-black text-white flex justify-center items-center text-5xl'>Tailwind working properly</div>
+    <div className='w-screen h-screen flex justify-center items-center bg-crust'>Tailwind working properly</div>
   )
 }
 
