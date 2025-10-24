91a924e0d0c20fbd3f347f2d281862173fe1c8f8
binit <binitgupta.1711@gmail.com>
2025-10-24 22:54:34 +0530

    Initial react setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-black text-white flex justify-center items-center text-5xl'>Tailwind working properly</div>
+  )
+}
+
+export default App
