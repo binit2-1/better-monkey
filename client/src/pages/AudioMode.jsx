428da1db3383bbfb8b8443ead6d5e58595310491
binit <binitgupta.1711@gmail.com>
2025-11-07 00:43:36 +0530

    add audio mode and reference mode pages

---
+++ client/src/pages/AudioMode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AudioMode = () => {
+  return (
+    <div>AudioMode</div>
+  )
+}
+
+export default AudioMode
---
+++ client/src/pages/ReferenceMode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReferenceMode = () => {
+  return (
+    <div>ReferenceMode</div>
+  )
+}
+
+export default ReferenceMode
