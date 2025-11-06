a7aa0cd7d54b2ac8fd38d80fdad7f7f2dbb30dcf
binit <binitgupta.1711@gmail.com>
2025-11-07 00:42:39 +0530

    change hover color

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className='grid grid-rows-[repeat(3,1fr)] h-full'>
         <div className='w-full h-full group cursor-pointer relative overflow-hidden'>
           {/* animated background: starts at 0 width and expands from right to left */}
-          <div className="absolute inset-0 bg-overlay0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out origin-right z-0" />
+          <div className="absolute inset-0 bg-surface0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out origin-right z-0" />
           <div className="relative z-10 w-full h-full flex items-center justify-center">
             <span className='pointer-events-none absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-90 origin-center whitespace-nowrap'>
               STANDARD
@@ -15,7 +15,7 @@ const Navbar = () => {
         </div>
 
         <div className='w-full h-full group cursor-pointer relative overflow-hidden'>
-          <div className="absolute inset-0 bg-overlay0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out origin-right z-0" />
+          <div className="absolute inset-0 bg-surface0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out origin-right z-0" />
           <div className="relative z-10 w-full h-full flex items-center justify-center">
             <span className='pointer-events-none absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-90 origin-center whitespace-nowrap'>
               REFERENCE
@@ -24,7 +24,7 @@ const Navbar = () => {
         </div>
 
         <div className='w-full h-full group cursor-pointer relative overflow-hidden'>
-          <div className="absolute inset-0 bg-overlay0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out origin-right z-0" />
+          <div className="absolute inset-0 bg-surface0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out origin-right z-0" />
           <div className="relative z-10 w-full h-full flex items-center justify-center">
             <span className='pointer-events-none absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-90 origin-center whitespace-nowrap'>
               AUDIO
